e1b1c09583f45da34b00bfbfd42a61e685278a0b
Yerin-Shin123 <yerin24680@naver.com>
2024-09-26 00:21:49 +0900

    week3-4

--- week3/week3.4.c
+++ week3/week3.4.c
@@ -4,16 +4,11 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	
-	float num1;
-	float num2;
+	float num1, num2;
 	
 	printf("enter two integers:");
 	scanf("%f,%f",&num1,&num2);
 	
-	printf("%f/%f= %f",num1,num2,num1/num2);
-	
-
-	
+	printf("%f/%f=%f",num1,num2,num1/num2);
 	return 0;
 }
