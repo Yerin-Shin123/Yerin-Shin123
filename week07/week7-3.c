3a118ea14b84f05674fce7a50f77c84b7d785942
Yerin-Shin123 <yerin24680@naver.com>
2024-10-16 14:27:51 +0900

    week7-3

---
+++ week07/week7-3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+/*int main(int argc, char *argv[]) 
+void square(int a)
+{
+	a=a*a;
+}
+	
+
+
+int main()
+{
+	int a=2;
+	square(a);
+	printf("a=%i\n",a);
+}
+*/
+
+int square(int a)
+{
+	return(a*a);
+}
+
+int main()
+{
+	int a=2;
+	a=square(a);
+	printf("a=%i\n",a);
+}
