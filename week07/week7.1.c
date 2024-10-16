8f6016d4e4abbfb04690ed6ef20e1036380a3c0b
Yerin-Shin123 <yerin24680@naver.com>
2024-10-16 13:47:47 +0900

    week7.1

--- week07/week7.1.c
+++ week07/week7.1.c
@@ -3,7 +3,8 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char *argv[]) {
+
+/*int main(int argc, char *argv[]) {
 	int i;
 	
 	for(i=0; i<10; i++)
@@ -12,6 +13,23 @@ int main(int argc, char *argv[]) {
 	  printf("*");
 	for(i=0; i<10; i++)
 	  printf("*");
-	
+	  
 	return 0;
+}
+*/
+
+void print_star()
+{
+	int i;
+	for(i=0; i<10; i++)
+	  printf("*");
+}
+
+int main(void)
+{
+	print_star();
+	print_star();
+	print_star();
+	
+  return 0;
 }
