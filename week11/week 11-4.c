fa28946999e68cebc4c9d7c8af733089bf69f710
Yerin-Shin123 <yerin24680@naver.com>
2024-11-24 22:28:50 +0900

    week 11-4

---
+++ week11/week 11-4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+void main(void)
+{
+	char str[30]="happy C programming";
+	
+	printf("words\"%s\"'s length:%i",str,strlen(str));
+
+}
